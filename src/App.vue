9b5980fe21a1f99528f372345676ad3b4726a7c1
Adam Sommer <adam@thehoick.com>
2018-05-26 08:37:31 -0400

    Can save URL into localStorage.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div>
     <h1 class="title">Evergreen New Releases</h1>
-    <Menu />
+    <Menu v-on:urlSet="urlSet" storedUrl=this.url />
     <hr/>
+
+    <p>Url: {{url}}</p>
   </div>
 </template>
 
 <script>
-  import Menu from './components/Menu';
-  
-  export default {
-    name: 'App',
-    components: {
-      Menu
-    }
-  };
+ import Menu from './components/Menu';
+ const SEARCH_URL = '/opac/extras/opensearch/1.1?searchTerms=thor&searchClass=keyword&count=25';
+ 
+ export default {
+   name: 'App',
+   data() {
+     return {
+       url: ''
+     }
+   },
+   created() {
+     this.url = localStorage.getItem('evergreen_url');
+   },
+   components: {
+     Menu
+   },
+   methods: {
+     urlSet(url) {
+       console.log('urlSet url:', url, SEARCH_URL); /* eslint-disable-line no-console */
+       this.url = url;
+     }
+   }
+ };
 </script>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="settings float-right">
-    <button class="button" type="button" data-toggle="settings-dropdown">&#x205E</button>
+    <button class="button" type="button" data-toggle="settings-dropdown">&#x205E;</button>
     <div class="dropdown-pane" id="settings-dropdown" data-dropdown data-auto-focus="true">
-      <input name="server-url" type="text" value="" placeholder="Server URL" @:input="onInput" />
+      <label for="server-url">Library URL</label>
+      <input name="server-url" id="server-url" type="text" value="{{props.localUrl}}" placeholder="http://... " v-on:keyup.enter="onSubmit" />
     </div>
   </div>
 </template>
@@ -10,9 +11,15 @@
 <script> 
  export default {
    name: 'Menu',
+   props: [
+     localUrl
+   ],
    methods: {
-     onInput: (e) => {
-       console.warn('e.target.value:', e.target.value);
+     onSubmit(event) {
+       console.log('this:', this);  /* eslint-disable-line no-console */
+
+       localStorage.setItem('evergreen_url', event.target.value);
+       this.$emit('urlSet', event.target.value);
      }
    }
  }
