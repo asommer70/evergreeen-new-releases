e4000174587213712104adfc399f9be2c7150f85
Adam Sommer <adam@thehoick.com>
2018-06-03 19:48:45 -0400

    Have functions for getting data from Wikipedia for each library title.

--- server/server.py
+++ server/server.py
@@ -17,18 +17,63 @@ def api():
 
     # Get the search page.
     search_url = "/eg/opac/results?bool=and&qtype=keyword&contains=contains&query=&bool=and&qtype=title&contains=contains&query=&bool=and&qtype=author&contains=contains&query=&_adv=1&detail_record_view=0&fi%3Aitem_type=g&fi%3Avr_format=v&locg=" + str(library_number) + "&pubdate=is&date1=&date2=&sort=pubdate.descending"
-    r = requests.get(base_url + search_url)
+    res = requests.get(base_url + search_url)
 
     # Find the .record_title.search_link elements.
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(res.text, 'html.parser')
     titles = soup.find_all('a', class_='record_title search_link')
-    titles = [title.string.split('[videorecording]')[0].strip() for title in titles]
+    titles = [title.string.split('[videorecording]')[0].strip()
+              for title in titles]
 
-    # Use subprocesses to get additional info for each title.
-    c = delegator.run('')
-    
-    return jsonify({
-        'message': 'Hello, World!',
-        'search_url': search_url,
-        'titles': titles
-    })
+    # Use coroutines to get additional info for each title.
+    dvds = []
+    for title in titles:
+        dvds.append({'title': title, 'wiki_matches': search_wiki(title)})
+        
+    return jsonify(dvds)
+
+
+def search_wiki(title):
+    url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + title + ' film'
+    res = requests.get(url)
+    data = res.json()
+
+    hits = []
+    for idx, hit in enumerate(data[1]):
+        hits.append({
+            'title': hit,
+            'description': data[2][idx],
+            'wiki_url': data[3][idx],
+            'image': get_image(hit)
+        })
+
+    print('hits:', hits)
+    # return {
+    #     'title': title,
+    #     'description:' description,
+    #     'wiki_url': wiki_url
+    # }
+    return hits
+
+
+def get_image(title):
+    url = 'https://en.wikipedia.org/w/api.php?action=query&prop=pageprops&format=json&titles=' + title
+    res = requests.get(url)
+    data = res.json()
+
+    page_id = [key for key in data['query']['pages']][0]
+    image_url = ''
+    try:
+        image_name = data['query']['pages'][page_id]['pageprops']['page_image']
+        
+        image_page_url = 'https://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&iiprop=url&format=json&titles=Image:' + image_name
+        print('image_page_url:', image_page_url)
+        image_res = requests.get(image_page_url)
+        image_data = image_res.json()
+        image_page_id = [key for key in image_data['query']['pages']][0]
+        print('image_page_id:', image_page_id)
+        image_url = image_data['query']['pages'][image_page_id]['imageinfo'][0]['url']
+    except KeyError:
+        pass
+
+    return image_url
